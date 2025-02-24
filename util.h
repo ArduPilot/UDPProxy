fece09c2dc36b206a98a483c9a0568c603d7a8b7
Andrew Tridgell <andrew@tridgell.net>
2025-02-25 07:11:09 +1100

    tidy socket handling
    fixes issue with socket TIME_WAIT

--- util.h
+++ util.h
@@ -1,6 +1,7 @@
 double time_seconds(void);
 int open_socket_in_udp(int port);
 int open_socket_in_tcp(int port);
+void set_tcp_options(int fd);
 const char *addr_to_str(struct sockaddr_in &addr);
 const char *time_string(void);
 
